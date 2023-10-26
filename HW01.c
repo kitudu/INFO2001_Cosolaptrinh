43e7831156072054831eb6afdd9b53a3678c6f8f
Your Name <you@example.com>
2023-10-26 16:48:00 +0700

    Ghi chu vao day

--- HW01.c
+++ HW01.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-int main(){
-	printf("Toi go lenh khac");
-	int i=0;
-	printf("%d",i); 
-	return 0; 
-} 
+
+int main() {
+	//Input
+	int a, b;
+	a = b = 3;
+
+
+	return(0);
+}
